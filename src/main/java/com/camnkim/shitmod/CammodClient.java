861c30f32946d7eabf79a96c752dc29fcae547ab
Cameron Kim <cameronnkim2000@gmail.com>
2025-06-20 00:46:20 -0700

    Initial commit

---
+++ src/main/java/com/camnkim/shitmod/CammodClient.java
@@ -0,0 +1,10 @@
+package com.camnkim.shitmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CammodClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/com/camnkim/shitmod/CammodDataGenerator.java
@@ -0,0 +1,11 @@
+package com.camnkim.shitmod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class CammodDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
